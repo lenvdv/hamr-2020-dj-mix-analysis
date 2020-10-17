d5bc96263c6136f3c379ac666428dda29d9e35b0
Romero <rapharomero@hotmail.fr>
2020-10-17 17:59:39 +0200

    edit visualization

--- main.py
+++ main.py
@@ -21,14 +21,21 @@ from src.visualization import plot_energy_band, plot_loudness, plot_spectral_com
 
 
 def main(input_path=None,
-         output_path=None):
+         output_path=None,
+         output_csv=None):
+    file_prefix = input_path.split(".wav")[0]
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path)
+    # 
+    # spectral_complexity_df = extract_spectral_complexity(input_path)
+    # plot_spectral_comp(spectral_complexity_df, outpath=os.path.join(output_path, "spectral_comp.html"))
 
     energy_band_df = extract_energy_bands(input_path)
-    plot_energy_band(df, outpath=os.path.join(output_path, "energy_band.html"))
-
-    # Loudness
-    loudness_df = extract_loudness(file)
-    plot_loudness(loudness_df, outpath=os.path.join(output_path, "loudness.html"))
+    plot_energy_band(energy_band_df, outpath=os.path.join(output_path, "energy_band.html"))
+    #
+    # # Loudness
+    # loudness_df = extract_loudness(input_path)
+    # plot_loudness(loudness_df, outpath=os.path.join(output_path, "loudness.html"))
 
 
 if(__name__ == "__main__"):
@@ -39,11 +46,17 @@ if(__name__ == "__main__"):
     parser.add_argument('-o', '--output_path',
                         default='output/',
                         help="Path where to store the data frame")
+    parser.add_argument('-csv', '--output-csv',
+                        default='output/csv/',
+                        help="Where to save the csvs containing descriptors"
+                        )
 
     args = parser.parse_args()
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
+
     main(
         input_path=args.input_path,
-        output_path=args.output_path
+        output_path=args.output_path,
+        output_csv=args.output_csv
     )
